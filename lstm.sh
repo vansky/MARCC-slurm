bc3192d7772628aabeda70edfc1f212b04c7b8e3
vansky <vansky@users.noreply.github.com>
2019-01-10 16:15:31 -0500

    updated module syntax

--- lstm.sh
+++ lstm.sh
@@ -12,9 +12,8 @@
 
 #EXCLUDE: git python
 module load cuda/8.0
-module load cudnn/6.0
 
-module load anaconda-python/3.6
+module load python/3.6-anaconda
 ## For first time use:
 #     conda create -n pytorchlm
 #     source activate pytorchlm
